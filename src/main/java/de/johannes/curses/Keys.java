8e8fbf6ee43fed2e1f86b0cfd57bf7317bfb2512
CommandJoo <hansjohannes118@gmail.com>
2024-12-19 11:58:33 +0100

    [+] Added all control keys

--- src/main/java/de/johannes/curses/Keys.java
+++ src/main/java/de/johannes/curses/Keys.java
@@ -6,8 +6,32 @@ public class Keys {
     public static final int ESCAPE = 27;
     public static final int BACK_SPACE = 263;
 
-    public static final int CTRL_X = 24;
+    public static final int CTRL_A = 1;
+    public static final int CTRL_B = 2;
+    public static final int CTRL_C = 3;
+    public static final int CTRL_D = 4;
+    public static final int CTRL_E = 5;
+    public static final int CTRL_F = 6;
+    public static final int CTRL_G = 7;
+    public static final int CTRL_H = 8;
+    public static final int CTRL_I = 9;
+    public static final int CTRL_J = 10;
+    public static final int CTRL_K = 11;
+    public static final int CTRL_L = 12;
+    public static final int CTRL_M = 13;
+    public static final int CTRL_N = 14;
+    public static final int CTRL_O = 15;
+    public static final int CTRL_P = 16;
+    public static final int CTRL_Q = 17;
+    public static final int CTRL_R = 18;
     public static final int CTRL_S = 19;
+    public static final int CTRL_T = 20;
+    public static final int CTRL_U = 21;
+    public static final int CTRL_V = 22;
+    public static final int CTRL_W = 23;
+    public static final int CTRL_X = 24;
+    public static final int CTRL_Y = 25;
+    public static final int CTRL_Z = 26;
 
     public static final int KEY_DOWN = 258;
     public static final int KEY_UP = 259;
